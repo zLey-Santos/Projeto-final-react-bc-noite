ffbf32f955929601d6c9c9a201cba88bb2d58ba0
zLey-Santos <wy.santos92@gmail.com>
2023-06-08 11:23:58 +0100

    delete

--- notepads-react-project/src/components/ErrorMessage.tsx
+++
@@ -1,7 +0,0 @@
-export type ErrorMessageProps = {
-  children: React.ReactNode;
-};
-
-export function ErrorMessage({ children }: ErrorMessageProps) {
-  return <span className="text-sm leading-tight text-red-500">{children}</span>;
-}
